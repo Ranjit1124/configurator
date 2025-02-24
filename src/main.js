a087dc4fae328fdec2bf4cbea1a0298bdf3a1e76
Ranjit1124 <ranjitpr1124@gmail.com>
2025-02-24 11:55:35 +0530

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,36 @@
+// import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+// import router from './router'
+import { createVuetify} from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+// import store from './Store';
+// import axios from 'axios';
+// import { VNumberInput } from 'vuetify/labs/VNumberInput'
+const vuetify = createVuetify({
+    components:{
+      ...components,
+      
 
-createApp(App).mount('#app')
+    },
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi,
+        },}
+  });
+  
+const app = createApp(App)
+// app.config.globalProperties.$axios = axios;
+
+// app.config.globalProperties.$axios = axios; 
+// app.use(store);
+// app.use(router)
+app.use(vuetify)
+app.mount('#app')
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template >
+    <v-card class="bg-red" >
+        <v-card-title>hii</v-card-title>
+    </v-card>
+</template>
+<script>
+export default {
+    name:'indexPage'
+}
+</script>
